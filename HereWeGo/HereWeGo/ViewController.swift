eb2230bae12f06fcac3c94abcd1f45757eb57e90
kongtianxintang <kongtianxintang@163.com>
2022-10-22 22:17:25 +0800

    first commit

---
+++ HereWeGo/HereWeGo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HereWeGo
+//
+//  Created by liwei on 2022/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
